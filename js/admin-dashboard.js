1e5981cf8e8928c6cb42a1cb2df5e8c3f19f3509
Vanshika Goel <vanshikagoel5002@gmail.com>
2026-09-10 12:10:54 +0530

    Added admin dashboard

---
+++ js/admin-dashboard.js
@@ -0,0 +1,15 @@
+function blockUser() {
+    alert("User blocked successfully!");
+}
+
+function approveProperty() {
+    alert("Property approved successfully!");
+}
+
+function removeProperty() {
+    alert("Property removed successfully!");
+}
+
+function resolveComplaint() {
+    alert("Complaint resolved successfully!");
+}
